abcbdaf33334e208e31a999a08c563f395a1f5e9
dhruvjit <dhruvjit@keito.works>
2020-04-29 22:34:50 +0530

    initial setup and first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import styles from './App.css';
+
+class App extends Component{
+
+    render(){
+        return(
+            <div className="">
+                "Hello friend From App.js"
+            </div>
+        );
+    }
+}
+
+export default App;
